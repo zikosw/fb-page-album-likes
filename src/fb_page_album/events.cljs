b8e046fc261c6b71fda503e602d9fe09c9955767
Supakorn Warodom <supakorn@satang.com>
2017-10-13 00:46:35 +0700

    :tada: Add /photos to get page's photo

--- src/fb_page_album/events.cljs
+++ src/fb_page_album/events.cljs
@@ -42,19 +42,25 @@
 (defn photo-url [album-id]
   (str fb-graph-url album-id "/photos"))
 
-(def photo-req
-  {:fields "event,link,picture,place,target,updated_time"})
 
-(defn get-photos [album]
-  (let [img (get-in album [:photos :data])]
-    (map #(get-in % [:images 0 :source]) img)))
+
+(defn get-photos [images]
+  (get-in images [:images 0 :source]))
+
+(def t
+  {:likes {:data [], :summary {:total_count 43, :can_like false, :has_liked false}}, :images [{:height 639, :source "https://scontent.xx.fbcdn.net/v/t1.0-9/21231906_1449885198432870_4276172318398922158_n.jpg?oh=b9bcd8b6993111b77c1a46ba1521959a&oe=5A3B42B5", :width 960} {:height 600, :source "https://scontent.xx.fbcdn.net/v/t1.0-0/p600x600/21231906_1449885198432870_4276172318398922158_n.jpg?oh=e06bbe0a12914a6716f0d988335a5b7a&oe=5A79FC87", :width 901} {:height 480, :source "https://scontent.xx.fbcdn.net/v/t1.0-0/p480x480/21231906_1449885198432870_4276172318398922158_n.jpg?oh=4163bc99a4915248632b4bd5b0153488&oe=5A79E8AE", :width 721} {:height 320, :source "https://scontent.xx.fbcdn.net/v/t1.0-0/p320x320/21231906_1449885198432870_4276172318398922158_n.jpg?oh=ae6f0e293371886ba55f499670311d0a&oe=5A7C04E9", :width 480} {:height 540, :source "https://scontent.xx.fbcdn.net/v/t1.0-0/p180x540/21231906_1449885198432870_4276172318398922158_n.jpg?oh=e5dde8eb45317527e10087d2834d5cb1&oe=5A6D21A2", :width 811} {:height 130, :source "https://scontent.xx.fbcdn.net/v/t1.0-0/p130x130/21231906_1449885198432870_4276172318398922158_n.jpg?oh=348d21adff28d2b9b4d05d1e76bedc20&oe=5A6EF50E", :width 195} {:height 225, :source "https://scontent.xx.fbcdn.net/v/t1.0-0/p75x225/21231906_1449885198432870_4276172318398922158_n.jpg?oh=d4f492cb7092fc8f9a7eec1a4d55c175&oe=5A6F45B9", :width 338}], :id "1449885198432870"})
+
+
+(defn get-photos-in-album [album]
+  (let [images (get-in album [:photos :data])]
+    (map #(get-photos %) images)))
 
 (defn get-likes [album]
   (get-in album [:likes :summary :total_count]))
 
 (comment
   (let [d (get-in data [:data 1])]
-    [(get-photos d) (get-likes d)]))
+    [(get-photos-in-album d) (get-likes d)]))
 
 (defn fetch-page-album [page-id after]
   (let [albums (chan)
@@ -65,8 +71,8 @@
                                                      "limit" 50
                                                      "fields" album-req
                                                      "after" after}}))  ;; MAX is 50
-                                                    ;; TODO: Implement paging
-                                                    ;;"after" "MTM5NjY4NjU4NzEwMzQzNQZDZD"}}))
+              ;; TODO: Implement paging
+              ;;"after" "MTM5NjY4NjU4NzEwMzQzNQZDZD"}}))
               res-status (:status response)
               body (:body response)
               paging (:paging body)
@@ -77,7 +83,7 @@
                             :name (:name a)
                             :desc (:description a)
                             :likes (get-likes a)
-                            :cover (first (get-photos a))})
+                            :cover (first (get-photos-in-album a))})
                          data)]
           (prn :status res-status)
           (prn :p paging)
@@ -106,6 +112,65 @@
 
 (rf/reg-event-fx
   :page/set-albums
+  (fn [{:keys [db]} [_ page-id albums]]
+    {:db (assoc db :albums albums)}))
+
+(defn fetch-page-photos [page-id after]
+  (let [photos (chan)
+        access-token (rf/subscribe [:api/get-access-token])]
+    (go (let [response (<! (http/get (photo-url page-id)
+                                     {:with-credentials? false
+                                      :query-params {"access_token" @access-token
+                                                     "limit" 50
+                                                     "fields" "likes.summary(true).limit(0),images,name"
+                                                     "type" "uploaded"
+                                                     "after" after}}))  ;; MAX is 50
+                                                    ;; TODO: Implement paging
+                                                    ;;"after" "MTM5NjY4NjU4NzEwMzQzNQZDZD"}}))
+              res-status (:status response)
+              body (:body response)
+              paging (:paging body)
+              data (:data body)
+              mapped-a (map
+                         (fn [a]
+                           {:id (:id a)
+                            :name (:name a)
+                            :likes (get-likes a)
+                            :cover (get-photos a)})
+                         data)]
+          (prn :status res-status)
+          (prn :p paging)
+          (prn :d (count data))
+          ;(prn :data mapped-a)
+          (doseq [p mapped-a]
+            (prn :ppp p))
+          (>! photos mapped-a)))
+    photos))
+
+(comment
+  (fetch-page-photos "bnk48official.cherprang" nil))
+
+(rf/reg-fx
+  :page/fetch-photos
+  (fn [{:keys [page-id on-success on-failed]}]
+    ;; TODO: Implement paging loop
+    (go
+      (on-success (<! (fetch-page-photos page-id nil))))))
+
+(comment
+  (rf/dispatch [:page/get-photos "IRoamAlone"])
+  (rf/dispatch [:page/get-photos "bnk48official.cherprang"])
+  (rf/dispatch [:page/get-photos "bnk48unclefan"]))
+
+(rf/reg-event-fx
+  :page/get-photos
+  (fn [db [_ page-id]]
+    (prn :get-photos page-id)
+    {:page/fetch-photos {:page-id page-id
+                         :on-success #(rf/dispatch [:page/set-photos page-id %])}}))
+
+(rf/reg-event-fx
+  :page/set-photos
   (fn [{:keys [db]} [_ page-id albums]]
     {:db (assoc db :albums albums)}))
 
